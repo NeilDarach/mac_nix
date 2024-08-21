aee74bcde8805dccde0b09db1ce1a9e254b2e784
Neil Darach <neil@Neils-Virtual-Machine.local>
2024-08-21 22:15:04 +0100

    Loading nvim from a flake

--- flake.nix
+++ flake.nix
@@ -6,29 +6,43 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     darwin.url = "github:lnl7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nixCats.url = "github:BirdeeHub/nixCats-nvim";
   };
 
-  outputs = inputs@{nixpkgs,home-manager,darwin,...}: {
-    darwinConfigurations.Neils-Virtual-Machine = darwin.lib.darwinSystem {
+  outputs = inputs@{nixpkgs,home-manager,darwin, nixCats, ...}: 
+    let 
       system = "aarch64-darwin";
-      pkgs = import nixpkgs { system = "aarch64-darwin"; };
-      modules = [ 
-        ./modules/darwin 
-        home-manager.darwinModules.home-manager {
-          users.knownUsers = [ "neil" ];
-          users.users.neil.home = "/Users/neil";
-          users.users.neil.uid = 501;
-          users.users.neil.shell = "/run/current-system/sw/bin/fish";
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            extraSpecialArgs = { inherit inputs; };
-            users.neil.imports = [
-              ./modules/home-manager 
-              ];
-            }; 
-          }
-        ];
+    in
+    {
+    darwinConfigurations = {
+      Neils-Virtual-Machine = darwin.lib.darwinSystem {
+      pkgs = import nixpkgs { 
+               system = "aarch64-darwin";
+               config = {
+                 allowUnfree = true;
+                 allowUnfreePredicate = _: true;
+                 };
+               };
+        specialArgs = { inherit inputs ; };
+        modules = [ 
+          ./modules/darwin 
+
+          home-manager.darwinModules.home-manager {
+            users.knownUsers = [ "neil" ];
+            users.users.neil.home = "/Users/neil";
+            users.users.neil.uid = 501;
+            users.users.neil.shell = "/run/current-system/sw/bin/fish";
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              extraSpecialArgs = { inherit inputs; };
+              users.neil.imports = [
+                ./modules/home-manager 
+                ];
+              }; 
+            }
+          ];
+        };
       }; 
     };
   }
--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,4 +1,11 @@
-{ pkgs, ...}: {
+{ inputs,pkgs, ...}: 
+let
+  nixCats = builtins.getFlake github:NeilDarach/nixNvim?rev=ac75b1c0d405e61e9cb8e1b7b2a5be003c542f53;
+in
+{
+  imports = [
+    nixCats.nixosModules.default
+  ];
   # darwin preferences and config
   nix.extraOptions = ''
     experimental-features = nix-command flakes
@@ -14,7 +21,7 @@
     '';
   environment.shells = with pkgs; [ fish bash zsh ];
   environment.loginShell = pkgs.fish;
-  environment.systemPackages = [ pkgs.coreutils ];
+  environment.systemPackages = [ pkgs.coreutils nixCats.packages.${pkgs.system}.nvim ];
   system.keyboard.enableKeyMapping = true;
   fonts.packages = [ (pkgs.nerdfonts.override { fonts = [ "SourceCodePro" "Meslo" ]; }) ];
   services.nix-daemon.enable = true;
@@ -25,6 +32,7 @@
     finder._FXShowPosixPathInTitle = true;
     NSGlobalDomain.InitialKeyRepeat = 14;
     NSGlobalDomain.KeyRepeat = 5;
+    NSGlobalDomain."com.apple.swipescrolldirection" = false;
     dock.persistent-apps = [ 
       "/Applications/Safari.app" 
       "${pkgs.alacritty}/Applications/Alacritty.app"
