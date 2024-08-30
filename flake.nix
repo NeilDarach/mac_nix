16f7b5afe93d1a257e4eb75b04aa628ac25f6771
Neil Darach <neil.darach@gmail.com>
2024-08-30 18:13:33 +0100

    Switched to the lze branch of my nvim config

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-# comment
+  # comment
   description = "Minimal Mac flake";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
@@ -7,60 +7,55 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     darwin.url = "github:lnl7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
-    nixNvim.url = "github:NeilDarach/nixNvim";
+    nixNvim.url = "github:NeilDarach/nixNvim/lze";
   };
 
-  outputs = inputs@{nixpkgs,home-manager,darwin, nixNvim, ...}: 
-    let 
-      darwin = 
-        let 
-          src = nixpkgs.legacyPackages."aarch64-darwin".applyPatches {
-            name = "nix-darwin";
-            src = inputs.darwin;
-            patches = [
-              ./patches/dock.nix.patch
-              ];
-            };
-        in
-          nixpkgs.lib.fix (self: (import "${src}/flake.nix").outputs { inherit self nixpkgs; });
+  outputs = inputs@{ nixpkgs, home-manager, darwin, nixNvim, ... }:
+    let
+      darwin = let
+        src = nixpkgs.legacyPackages."aarch64-darwin".applyPatches {
+          name = "nix-darwin";
+          src = inputs.darwin;
+          patches = [ ./patches/dock.nix.patch ];
+        };
+      in nixpkgs.lib.fix
+      (self: (import "${src}/flake.nix").outputs { inherit self nixpkgs; });
       system = "aarch64-darwin";
-    in
-    {
-    darwinConfigurations = {
-      Neils-Virtual-Machine = darwin.lib.darwinSystem {
-      pkgs = import nixpkgs { 
-               system = "aarch64-darwin";
-               config = {
-                 allowUnfree = true;
-                 allowUnfreePredicate = _: true;
-                 };
-               };
-        specialArgs = { inherit inputs ; };
-        modules = [ 
-          ./modules/darwin 
+    in {
+      darwinConfigurations = {
+        Neils-Virtual-Machine = darwin.lib.darwinSystem {
+          pkgs = import nixpkgs {
+            system = "aarch64-darwin";
+            config = {
+              allowUnfree = true;
+              allowUnfreePredicate = _: true;
+            };
+          };
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./modules/darwin
 
-          home-manager.darwinModules.home-manager {
-            users = {
-	      knownUsers = [ "neil" ];
+            home-manager.darwinModules.home-manager
+            {
               users = {
-	        neil = {
-		  home = "/Users/neil";
-                  uid = 501;
-                  shell = "/run/current-system/sw/bin/fish";
-		  };
-		};
-	      };
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              extraSpecialArgs = { inherit inputs; };
-              users.neil.imports = [
-                ./modules/home-manager
-                ];
-              }; 
+                knownUsers = [ "neil" ];
+                users = {
+                  neil = {
+                    home = "/Users/neil";
+                    uid = 501;
+                    shell = "/run/current-system/sw/bin/fish";
+                  };
+                };
+              };
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                extraSpecialArgs = { inherit inputs; };
+                users.neil.imports = [ ./modules/home-manager ];
+              };
             }
           ];
         };
-      }; 
+      };
     };
-  }
+}
