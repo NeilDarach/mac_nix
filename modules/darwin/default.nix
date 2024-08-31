614b80b374457bb82ebccbd48f0322ce28bd57b2
Neil Darach <neil.darach@gmail.com>
2024-08-31 06:31:31 +0100

    Updated flake.lock.  Shell function for updating a single input

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,20 +1,16 @@
-{ inputs,pkgs, ...}: 
-let
-  wallpaper = ./wallpaper.plist;
-in
-{
-  imports = [
-    inputs.nixNvim.nixosModules.default
-  ];
+{ inputs, pkgs, ... }:
+let wallpaper = ./wallpaper.plist;
+in {
+  imports = [ inputs.nixNvim.nixosModules.default ];
   # darwin preferences and config
   environment.etc = {
     "sudoers.d/10-nix-commands".text = ''
       neil ALL=(root) NOPASSWD:SETENV: /run/current-system/sw/bin/nix-env 
-      '';
-    };
+    '';
+  };
   nix.extraOptions = ''
     experimental-features = nix-command flakes
-    '';
+  '';
   programs.zsh.enable = true;
   programs.fish.enable = true;
   programs.fish.shellInit = ''
@@ -23,23 +19,31 @@ in
         set -g fish_user_paths $p $fish_user_paths
       end
     end
-    '';
+  '';
   environment.shells = with pkgs; [ fish bash zsh ];
   environment.loginShell = pkgs.fish;
-  environment.systemPackages = with pkgs; [ plistwatch jq coreutils perl python3 ruby gcc
- inputs.nixNvim.packages.${pkgs.system}.nvim
-];
+  environment.systemPackages = with pkgs; [
+    plistwatch
+    jq
+    coreutils
+    perl
+    python3
+    ruby
+    gcc
+    inputs.nixNvim.packages.${pkgs.system}.nvim
+  ];
   system.keyboard.enableKeyMapping = true;
-  fonts.packages = [ (pkgs.nerdfonts.override { fonts = [ "SourceCodePro" "Meslo" ]; }) ];
+  fonts.packages =
+    [ (pkgs.nerdfonts.override { fonts = [ "SourceCodePro" "Meslo" "NerdFontsSymbolsOnly" ]; }) ];
   services.nix-daemon.enable = true;
- # system.activationScripts.setLoginShell.text = ''
- #   sudo /usr/bin/chsh -s /run/current-system/sw/bin/fish neil
- #   '';
+  # system.activationScripts.setLoginShell.text = ''
+  #   sudo /usr/bin/chsh -s /run/current-system/sw/bin/fish neil
+  #   '';
   system.activationScripts.postUserActivation.text = ''
     /usr/libexec/PlistBuddy -c "Clear dict" -c "Merge ${wallpaper}" -c Save ~/Library/Application\ Support/com.apple.wallpaper/Store/Index.plist
     killall WallpaperAgent
     /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
-    '';
+  '';
 
   system.defaults = {
     finder._FXShowPosixPathInTitle = true;
@@ -47,14 +51,14 @@ in
     NSGlobalDomain.KeyRepeat = 5;
     NSGlobalDomain."com.apple.swipescrolldirection" = false;
     dock = {
-      persistent-apps = [ 
-        "/Applications/Safari.app" 
+      persistent-apps = [
+        "/Applications/Safari.app"
         "${pkgs.alacritty}/Applications/Alacritty.app"
         "${pkgs.neovide}/Applications/Neovide.app"
-        ];
+      ];
       persistent-others = [ "/Users/neil/Downloads" "/Applications" ];
       show-recents = false;
       orientation = "left";
-      };
     };
-  }
+  };
+}
--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,61 +1,62 @@
-              {pkgs,lib,...}: {
-                 # home-manger configs
-                 home.stateVersion = "22.11";
-                 home.packages = [ pkgs.ripgrep pkgs.fd pkgs.curl pkgs.less ];
-                 home.sessionVariables = {
-                   PAGER = "less";
-                   CLICOLOR = 1;
-                   EDITOR="vim";
-                   };
-                 home.shellAliases = {
-                   ls = "ls --color=auto -F";
-                   nr = "darwin-rebuild switch --flake ~/mac_nix/.#";
-                   };
+{ pkgs, lib, ... }: {
+  # home-manger configs
+  home.stateVersion = "22.11";
+  home.packages = [ pkgs.ripgrep pkgs.fd pkgs.curl pkgs.less ];
+  home.sessionVariables = {
+    PAGER = "less";
+    CLICOLOR = 1;
+    EDITOR = "vim";
+  };
+  home.shellAliases = {
+    ls = "ls --color=auto -F";
+    nr = "darwin-rebuild switch --flake ~/mac_nix/.#";
+  };
+
   programs.fish = {
-   enable = true;
-   loginShellInit = 
-     let
-       profiles = [
-         "/etc/profiles/per-user/$USER"
-         "$HOME/.nix-profile"
-         "(set -q XDG_STATE_HOME; and echo $XDG_STATE_HOME; or echo $HOME/.local/state)/nix/profile"
-         "/run/current-system/sw"
-         "/nix/var/nix/profiles/default"
-         ];
-       makeBinSearchPath = 
-         lib.concatMapStringsSep " " (path: "${path}/bin");
-      in
-    ''
-    fish_add_path --move --prepend --path ${makeBinSearchPath profiles}
-    set fish_user_paths $fish_user_paths  
-    fish_vi_key_bindings
+    enable = true;
+    functions = {
+            flake-update ="nix flake lock --update-input $argv[1]";
+        };
+    loginShellInit = let
+      profiles = [
+        "/etc/profiles/per-user/$USER"
+        "$HOME/.nix-profile"
+        "(set -q XDG_STATE_HOME; and echo $XDG_STATE_HOME; or echo $HOME/.local/state)/nix/profile"
+        "/run/current-system/sw"
+        "/nix/var/nix/profiles/default"
+      ];
+      makeBinSearchPath = lib.concatMapStringsSep " " (path: "${path}/bin");
+    in ''
+      fish_add_path --move --prepend --path ${makeBinSearchPath profiles}
+      set fish_user_paths $fish_user_paths  
+      fish_vi_key_bindings
     '';
-};
-                 programs = {
-                   bat.enable = true;
-                   bat.config.theme = "TwoDark";
-                   fzf.enable = true;
-                   fzf.enableZshIntegration = true;
-                   git = {
-                     enable = true;
-                     ignores = [
-                                 "*~"
-                                 "*.swp"
-                                 ];
-                     userEmail = "neil.darach@gmail.com";
-                     userName = "Neil Darach";
-                     };
-                   zsh.enable = true;
-                   zsh.enableCompletion = true;
-                   zsh.autosuggestion.enable = true;
-                   zsh.syntaxHighlighting.enable = true;
-                   starship.enable = true;
-                   starship.enableZshIntegration = true;
-                   starship.settings = { format = "$username$hostname$localip$shlvl$singularity$kubernetes$nats$directory$vcsh$fossil_branch$fossil_metrics$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch$pijul_channel$docker_context$package$bun$c$cmake$cobol$daml$dart$deno$dotnet$elixir$elm$erlang$fennel$gleam$golang$gradle$haskell$haxe$helm$java$julia$kotlin$lua$nim$nodejs$ocaml$odin$opa$perl$php$pulumi$purescript$python$quarto$raku$rlang$red$ruby$rust$scala$solidity$terraform$typst$vlang$vagrant$zig$buf$guix_shell$nix_shell$conda$meson$spack$memory_usage$aws$gcloud$openstack$azure$direnv$env_var$crystal$custom$sudo$cmd_duration$line_break$jobs$battery$time$status$container$os$shell$character"; };
-                   alacritty = {
-                     enable = true;
-                     settings.font.normal.family = "SauceCodePro Nerd Font Mono";
-                     settings.font.size = 16; 
-                     };
-                   };
-                }
+  };
+  programs = {
+    bat.enable = true;
+    bat.config.theme = "TwoDark";
+    fzf.enable = true;
+    fzf.enableZshIntegration = true;
+    git = {
+      enable = true;
+      ignores = [ "*~" "*.swp" ];
+      userEmail = "neil.darach@gmail.com";
+      userName = "Neil Darach";
+    };
+    zsh.enable = true;
+    zsh.enableCompletion = true;
+    zsh.autosuggestion.enable = true;
+    zsh.syntaxHighlighting.enable = true;
+    starship.enable = true;
+    starship.enableZshIntegration = true;
+    starship.settings = {
+      format =
+        "$username$hostname$localip$shlvl$singularity$kubernetes$nats$directory$vcsh$fossil_branch$fossil_metrics$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch$pijul_channel$docker_context$package$bun$c$cmake$cobol$daml$dart$deno$dotnet$elixir$elm$erlang$fennel$gleam$golang$gradle$haskell$haxe$helm$java$julia$kotlin$lua$nim$nodejs$ocaml$odin$opa$perl$php$pulumi$purescript$python$quarto$raku$rlang$red$ruby$rust$scala$solidity$terraform$typst$vlang$vagrant$zig$buf$guix_shell$nix_shell$conda$meson$spack$memory_usage$aws$gcloud$openstack$azure$direnv$env_var$crystal$custom$sudo$cmd_duration$line_break$jobs$battery$time$status$container$os$shell$character";
+    };
+    alacritty = {
+      enable = true;
+      settings.font.normal.family = "SauceCodePro Nerd Font Mono";
+      settings.font.size = 16;
+    };
+  };
+}
