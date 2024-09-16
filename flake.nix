a9e920ad0370a8d5f91dd8ec72ccc492b33b6024
Neil Darach <neil.darach@gmail.com>
2024-09-16 21:45:41 +0100

    Use the same configuration for the VM and the laptop

--- flake.nix
+++ flake.nix
@@ -17,6 +17,52 @@
   outputs =
     inputs@{ nixpkgs, nixpkgs-unstable, home-manager, darwin, nixNvim, ... }:
     let
+      standardMac = {
+        pkgs = import nixpkgs {
+          system = "aarch64-darwin";
+          overlays = [ inputs.firefox-darwin.overlay inputs.nur.overlay ];
+          config = {
+            allowUnfree = true;
+            allowUnfreePredicate = _: true;
+          };
+        };
+        specialArgs = {
+          inherit inputs;
+          pkgs-unstable = import nixpkgs-unstable {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        };
+        modules = [
+          ./modules/darwin
+          home-manager.darwinModules.home-manager
+          {
+            users = {
+              knownUsers = [ "neil" ];
+              users = {
+                neil = {
+                  home = "/Users/neil";
+                  uid = 501;
+                  shell = "/run/current-system/sw/bin/fish";
+                };
+              };
+            };
+            home-manager = {
+              backupFileExtension = "bak";
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              extraSpecialArgs = {
+                inherit inputs;
+                pkgs-unstable = import nixpkgs-unstable {
+                  inherit system;
+                  config.allowUnfree = true;
+                };
+              };
+              users.neil.imports = [ ./modules/home-manager ];
+            };
+          }
+        ];
+      };
       darwin = let
         src = nixpkgs.legacyPackages."aarch64-darwin".applyPatches {
           name = "nix-darwin";
@@ -30,52 +76,10 @@
 
     in {
       darwinConfigurations = {
-        Neils-Virtual-Machine = darwin.lib.darwinSystem {
-          pkgs = import nixpkgs {
-            system = "aarch64-darwin";
-            overlays = [ inputs.firefox-darwin.overlay inputs.nur.overlay ];
-            config = {
-              allowUnfree = true;
-              allowUnfreePredicate = _: true;
-            };
-          };
-          specialArgs = {
-            inherit inputs;
-            pkgs-unstable = import nixpkgs-unstable {
-              inherit system;
-              config.allowUnfree = true;
-            };
-          };
-          modules = [
-            ./modules/darwin
-            home-manager.darwinModules.home-manager
-            {
-              users = {
-                knownUsers = [ "neil" ];
-                users = {
-                  neil = {
-                    home = "/Users/neil";
-                    uid = 501;
-                    shell = "/run/current-system/sw/bin/fish";
-                  };
-                };
-              };
-              home-manager = {
-                backupFileExtension = "bak";
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                extraSpecialArgs = {
-                  inherit inputs;
-                  pkgs-unstable = import nixpkgs-unstable {
-                    inherit system;
-                    config.allowUnfree = true;
-                  };
-                };
-                users.neil.imports = [ ./modules/home-manager ];
-              };
-            }
-          ];
-        };
+
+        Neils-Virtual-Machine = darwin.lib.darwinSystem standardMac;
+        Neils-Macbook-Pro = darwin.lib.darwinSystem standardMac;
+
       };
       homeConfigurations."neil" = home-manager.lib.homeManagerConfiguration {
         pkgs = import nixpkgs {
