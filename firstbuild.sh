006881c4cd36239b06dfcd4c8fa5399415c129f1
Neil Darach <neil.darach@gmail.com>
2024-10-01 13:30:22 +0100

    Try to use the current hostname

--- firstbuild.sh
+++ firstbuild.sh
@@ -1,5 +1,7 @@
 which nix || (sh <(curl -L https://nixos.org/nix/install); exit)
 which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 softwareupdate --install-rosetta --agree-to-license
-nix --extra-experimental-features "nix-command flakes" build github:NeilDarach/mac_nix#darwinConfigurations.Neils-Virtual-Machine.system
-./result/sw/bin/darwin-rebuild switch --flake github:NeilDarach/mac_nix#
+
+nix --extra-experimental-features "nix-command flakes" build github:NeilDarach/mac_nix#darwinConfigurations.$(hostname -s).system
+
+./result/sw/bin/darwin-rebuild switch --flake github:NeilDarach/mac_nix#$(hostname -s)
